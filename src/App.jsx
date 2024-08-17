6be5853eb741f432b6d969b59ecbde05b5b7abf6
memooh <m.ovdb@outlook.com>
2024-08-17 13:56:34 +0200

    Added projects. Added new fonts, responsive check

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Hero from "./components/Hero/Hero";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Skills from "./components/Skills/Skills";
+import Project from "./components/Projects/Project";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <AboutMe />
       <Skills />
+      <Project />
     </>
   );
 }
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,10 +1,59 @@
 import './Project.css';
 
+import bookmarkd from '/images/projects/bookmarkd.png';
+import arshi from '/images/projects/arshi.png';
+import opulence from '/images/projects/opulence.png';
+import barbershop from '/images/projects/barbershop.jpg';
+
 const Project = () => {
+    const projects = [
+        {
+            id: 1,
+            title: 'BookMark\'d',
+            description: 'A bookmarking app that allows users to save and organize their favorite books.',
+            image: bookmarkd,
+            github: 'https://github.com/memooh/bookmarkd'
+        },
+        {
+            id: 2,
+            title: 'Arshi | Digital Agency',
+            description: 'A digital agency website that showcases the agency\'s work and services.',
+            image: arshi,
+            github: 'https://github.com/memooh/digital_agency'
+        },
+        {
+            id: 3,
+            title: 'Opulence',
+            description: 'An interior design website that showcases the companies work and services.',
+            image: opulence,
+            github: 'https://github.com/memooh/opulence'
+        },
+        {
+            id: 4,
+            title: 'Barbershop',
+            description: 'a barber shop website that showcases the barbershop\'s work and services.',
+            image: barbershop,
+            github: 'https://github.com/memooh/barbershop'
+        },
+    ]
     return (
-        <section className="projects">
+        <section className="projects mb-5">
             <div className="container">
-                <h1>yo</h1>
+                <h1 className='text-center projectsTitle'>Personal Projects</h1>
+                <div className="row">
+                    {projects.map((project) => (
+                        <div className="col-lg-6 col-md-6 mb-4" key={project.id}>
+                            <div className="project">
+                                <img src={project.image} alt={project.title} className="img-fluid" />
+                                <h3>{project.title}</h3>
+                                <p>{project.description}</p>
+                                <a href={project.github} target="_blank" rel="noopener noreferrer">
+                                    <button className="projectButton">View on GitHub</button>
+                                </a>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     )
