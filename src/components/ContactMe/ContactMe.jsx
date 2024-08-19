75a023f820de1d50efd28f055ce34665e8c9cf66
memooh <m.ovdb@outlook.com>
2024-08-19 14:37:09 +0200

    contactme linkedin url added

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,27 +1,27 @@
 import "./ContactMe.css";
 
-import linkedin from '/contact/linkedin.webp';
-import github from '/contact/github.png';
-import nosocialmedia from '/contact/nosocialmedia.png';
+import linkedin from "/contact/linkedin.webp";
+import github from "/contact/github.png";
+import nosocialmedia from "/contact/nosocialmedia.png";
 
 const ContactMe = () => {
-    const links = [
-        {
-            name: "LinkedIn",
-            url: "#",
-            icon: linkedin,
-        },
-        {
-            name: "GitHub",
-            url: "https://github.com/memooh",
-            icon: github,
-        },
-        {
-            name: "No other social media",
-            url: "#",
-            icon: nosocialmedia,
-        },
-    ];
+  const links = [
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/mehmet-emin-ozturk-04a21b324/",
+      icon: linkedin,
+    },
+    {
+      name: "GitHub",
+      url: "https://github.com/memooh",
+      icon: github,
+    },
+    {
+      name: "No other social media",
+      url: "#",
+      icon: nosocialmedia,
+    },
+  ];
 
   return (
     <section id="contactMeSection" className="contactMe ">
@@ -33,24 +33,28 @@ const ContactMe = () => {
         <h3 className="email mt-5">
           <a href="mailto:m.ovdb@outlook.com">m.ovdb@outlook.com</a>
         </h3>
-        <h1 className="mt-5">
-            You can also find me on:
-        </h1>
+        <h1 className="mt-5">You can also find me on:</h1>
         <div className="row mt-5">
-    {links.map((link, index) => (
-        <div key={index} className="col-md-4 mb-3">
-            <a href={link.url} target="_blank" rel="noopener noreferrer" className="social-link">
+          {links.map((link, index) => (
+            <div key={index} className="col-md-4 mb-3">
+              <a
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-link"
+              >
                 <div className="social-icon">
-                    <img src={link.icon} alt={link.name} className="social-icon-img" />
+                  <img
+                    src={link.icon}
+                    alt={link.name}
+                    className="social-icon-img"
+                  />
                 </div>
-                <div className="social-name mt-2">
-                    {link.name}
-                </div>
-            </a>
+                <div className="social-name mt-2">{link.name}</div>
+              </a>
+            </div>
+          ))}
         </div>
-    ))}
-</div>
-
       </div>
     </section>
   );
